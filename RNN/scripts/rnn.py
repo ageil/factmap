e609492dd9eed2cba236a0e312521c12d159d9aa
Anders Geil <anders.geil@gmail.com>
2019-05-18 14:48:14 -0700

    Added support for dropout

--- RNN/scripts/rnn.py
+++ RNN/scripts/rnn.py
@@ -1,12 +1,12 @@
 from keras.models import Model
-from keras.layers import Input, Embedding, LSTM
+from keras.layers import Input, Embedding, LSTM, Dropout
 from keras.layers.wrappers import Bidirectional
 from keras.initializers import Constant
 from keras.optimizers import Adam
 from keras.layers.merge import Add
 
 
-def singleRNN(embedding_matrix, trainable=False, lr=1e-3):
+def singleRNN(embedding_matrix, trainable=False, lr=1e-3, dropout=False):
 	rnn_in = Input(shape=(None,), name='rnn_in')
 	rnn = Embedding(input_dim=embedding_matrix.shape[0], 
 	                output_dim=embedding_matrix.shape[1], 
@@ -18,7 +18,11 @@ def singleRNN(embedding_matrix, trainable=False, lr=1e-3):
 	                name="embedding")(rnn_in)
 
 	rnn = Bidirectional(LSTM(256, return_sequences=True, name="lstm_1"), name="bi_lstm_1")(rnn)
+	if dropout:
+		rnn = Dropout(0.5)(rnn)
 	rnn = Bidirectional(LSTM(64, return_sequences=True, name="lstm_2"), name="bi_lstm_2")(rnn)
+	if dropout:
+		rnn = Dropout(0.5)(rnn)
 	rnn_out = LSTM(1, activation='sigmoid', return_sequences=False, name="lstm_out")(rnn)
 
 	rnn_single = Model(inputs=rnn_in,
@@ -28,7 +32,7 @@ def singleRNN(embedding_matrix, trainable=False, lr=1e-3):
 	            metrics=['accuracy'])
 	return rnn_single
 
-def dualRNN(embedding_matrix_inv, embedding_matrix_oov, trainable_inv=False, trainable_oov=True, lr=1e-3):
+def dualRNN(embedding_matrix_inv, embedding_matrix_oov, trainable_inv=False, trainable_oov=True, lr=1e-3), dropout=False:
 	rnn_in0 = Input(shape=(None,), name='rnn_in_inv')
 	rnn0 = Embedding(input_dim=embedding_matrix_inv.shape[0], 
 	                output_dim=embedding_matrix_inv.shape[1], 
@@ -51,7 +55,11 @@ def dualRNN(embedding_matrix_inv, embedding_matrix_oov, trainable_inv=False, tra
 
 	rnn = Add(name="add")([rnn0, rnn1])
 	rnn = Bidirectional(LSTM(256, return_sequences=True, name="lstm_1"), name="bi_lstm_1")(rnn)
+	if dropout:
+		rnn = Dropout(0.5)(rnn)
 	rnn = Bidirectional(LSTM(64, return_sequences=True, name="lstm_2"), name="bi_lstm_2")(rnn)
+	if dropout:
+		rnn = Dropout(0.5)(rnn)
 	rnn_out = LSTM(1, activation='sigmoid', return_sequences=False, name="lstm_out")(rnn)
 
 	rnn_dual = Model(inputs=[rnn_in0, rnn_in1],
